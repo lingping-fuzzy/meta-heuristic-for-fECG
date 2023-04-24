01bf165ce6774d42e87a7388acf6c7cb4bd2fd28
lingping kong <117819803+lingping-fuzzy@users.noreply.github.com>
2023-04-24 14:25:28 +0200

    Update basic_example_run.m

--- basic_example_run.m
+++ basic_example_run.m
@@ -1,3 +1,7 @@
+% this is an example code,
+% please read Readme, then check this code
+% parameter instructions is introduced in file [Fetal_problem.m]
+
 clear; clc;
 D = [30, 20, 22, 46, 30, 1, 46, 50, 64, 55]; 
 N = [10, 15, 20]; type=[1, 2]; %Two different names(obtain dynamic coeffient or constant)
@@ -13,7 +17,7 @@ for a = 1: size(alg, 2)
    for j = 1:10 % person_num
          pID = j;
          algo = str2func(alg{:, a});
-         platemo('algorithm',algo ,'problem',{@Fetal_cal_par,pID,mirror, type(Tc), mu(j)},'N',15, 'D', D(j),'maxFE', D(j)*50 , 'save', 2)
+         platemo('algorithm',algo ,'problem',{@Fetal_problem,pID,mirror, type(Tc), mu(j)},'N',15, 'D', D(j),'maxFE', D(j)*50 , 'save', 2)
    end
 end
 
